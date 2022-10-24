c9ac5c2a05d9ac9fc8ccf63b30850535850f754e
Conor Dunne <conordunne@outlook.ie>
2022-10-25 07:21:06 +0900

    Add highlight squares function for placing ships

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import "./styles.css";
 import GameboardFactory from "./Factories/gameboard";
 import ShipFactory from "./Factories/ship";
@@ -7,43 +8,13 @@ import renderBoard from "./components/boards";
 const playerboard = GameboardFactory("player");
 const computerBoard = GameboardFactory("computer");
 
-const playerShipFourA = ShipFactory("SizeFourA", 4);
-const playerShipFourB = ShipFactory("SizeFourB", 4);
-const playerShipThreeA = ShipFactory("SizeThreeA", 3);
-
-const computerShipFourA = ShipFactory("SizeFourA", 4);
-const computerShipFourB = ShipFactory("SizeFourB", 4);
-const computerShipThreeA = ShipFactory("SizeThreeA", 3);
-
-const human = Player("Conor", computerBoard);
-const computer = Player("computer", playerboard);
-
-playerboard.placeShip(playerShipFourA, "down", 2, 1);
-playerboard.placeShip(playerShipFourB, "across", 5, 5);
-playerboard.placeShip(playerShipThreeA, "across", 9, 6);
-
-computerBoard.placeShip(computerShipFourA, "across", 3, 4);
-computerBoard.placeShip(computerShipFourB, "down", 7, 4);
-computerBoard.placeShip(computerShipThreeA, "down", 2, 3);
-
-console.log(human.takeShot(3, 4));
-console.log(human.takeShot(3, 5));
-console.log(human.takeShot(3, 6));
-console.log(human.takeShot(3, 7));
-
-console.log(human.takeShot(7, 4));
-console.log(human.takeShot(8, 4));
-console.log(human.takeShot(9, 4));
-console.log(human.takeShot(10, 4));
-
-console.log(human.takeShot(2, 3));
-console.log(human.takeShot(3, 3));
-console.log(human.takeShot(4, 3));
-
 renderBoard("player-board");
 renderBoard("computer-board");
 
-const domShip = function (x, y, length) {
+const domShip = function (el, length) {
+  const x = el.target.dataset.coords[0];
+  const y = el.target.dataset.coords[2];
+  console.log(y);
   const placePoint = document.querySelector(
     `div#player-board > [data-coords = "${x},${y}"]`
   );
@@ -58,6 +29,26 @@ const domShip = function (x, y, length) {
   }
 
   placePoint.appendChild(ship);
+
+  setTimeout(() => {
+    placePoint.removeChild(ship);
+  }, 100);
 };
 
-domShip(3,1, 4);
+const highlight = function (el) {
+  el.classList.toggle("ship-four");
+};
+
+const unHighlight = function (el) {
+  el.classList.remove("ship-four");
+};
+
+const playerSquares = document.querySelectorAll("#player-board > .square");
+
+playerSquares.forEach((el) =>
+  el.addEventListener("mouseover", () => highlight(el))
+);
+
+playerSquares.forEach((el) =>
+  el.addEventListener("mouseout", () => unHighlight(el))
+);
