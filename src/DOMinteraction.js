31d1bbe6db4a4aedebacc62b63f12652561b0943
cdutd83 <cdutd83@gmail.comasd>
2022-10-26 15:13:41 +0900

    Add button to rotate axis when placing ships

---
+++ src/DOMinteraction.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-undef */
+let shipAxis = "Y";
+const axisBtn = document.getElementById("axis");
+
+export const highlight = function (el, length) {
+  let data = el.dataset.coords;
+  data = data.split(",");
+
+  if (shipAxis === "X") {
+    console.log(shipAxis);
+    for (let i = 0; i < length; i += 1) {
+      const square = document.querySelector(
+        `[data-coords="${data[0]},${Number(data[1]) + i}"]`
+      );
+      square.classList.add("ship-four");
+    }
+  } else {
+
+    for (let i = 0; i < length; i += 1) {
+      const square = document.querySelector(
+        `[data-coords="${Number(data[0]) + i},${data[1]}"]`
+      );
+      square.classList.add("ship-four");
+    }
+  }
+};
+
+export const unHighlight = function (el, length) {
+  let data = el.dataset.coords;
+  data = data.split(",");
+
+  if (shipAxis === "X") {
+    console.log(shipAxis);
+    for (let i = 0; i < length; i += 1) {
+      const square = document.querySelector(
+        `[data-coords="${data[0]},${Number(data[1]) + i}"]`
+      );
+      square.classList.remove("ship-four");
+    }
+  } else {
+
+    for (let i = 0; i < length; i += 1) {
+      const square = document.querySelector(
+        `[data-coords="${Number(data[0]) + i},${data[1]}"]`
+      );
+      square.classList.remove("ship-four");
+    }
+  }
+};
+
+const changeAxis = function () {
+  if (shipAxis === "Y") {
+    shipAxis = "X";
+  } else shipAxis = "Y";
+  console.log(shipAxis);
+};
+
+axisBtn.addEventListener("click", changeAxis);
--- src/index.js
+++ src/index.js
@@ -4,60 +4,40 @@ import GameboardFactory from "./Factories/gameboard";
 import ShipFactory from "./Factories/ship";
 import Player from "./Factories/player";
 import renderBoard from "./components/boards";
+import { highlight, unHighlight } from "./DOMinteraction";
 
 const playerboard = GameboardFactory("player");
 const computerBoard = GameboardFactory("computer");
 
+
 renderBoard("player-board");
 renderBoard("computer-board");
 
-const domShip = function (el, length) {
-  const x = el.target.dataset.coords[0];
-  const y = el.target.dataset.coords[2];
-  console.log(y);
-  const placePoint = document.querySelector(
-    `div#player-board > [data-coords = "${x},${y}"]`
-  );
+// const domShip = function (el, length) {
+//   const x = el.target.dataset.coords[0];
+//   const y = el.target.dataset.coords[2];
+//   console.log(y);
+//   const placePoint = document.querySelector(
+//     `div#player-board > [data-coords = "${x},${y}"]`
+//   );
 
-  const ship = document.createElement("div");
-  ship.classList.add("ship");
+//   const ship = document.createElement("div");
+//   ship.classList.add("ship");
 
-  for (let i = 1; i <= length; i += 1) {
-    const oneSquare = document.createElement("div");
-    oneSquare.classList.add("ship-square");
-    ship.appendChild(oneSquare);
-  }
+//   for (let i = 1; i <= length; i += 1) {
+//     const oneSquare = document.createElement("div");
+//     oneSquare.classList.add("ship-square");
+//     ship.appendChild(oneSquare);
+//   }
 
-  placePoint.appendChild(ship);
+//   placePoint.appendChild(ship);
 
-  setTimeout(() => {
-    placePoint.removeChild(ship);
-  }, 100);
-};
-
-const highlight = function (el, length) {
-  let data = el.dataset.coords;
-  data = data.split(",");
-  for (let i = 0; i < length; i += 1) {
-    const square = document.querySelector(
-      `[data-coords="${data[0]},${Number(data[1]) + i}"]`
-    );
-    square.classList.add("ship-four");
-  }
-};
-
-const unHighlight = function (el, length) {
-  let data = el.dataset.coords;
-  data = data.split(",");
-  for (let i = 0; i < length; i += 1) {
-    const square = document.querySelector(
-      `[data-coords="${data[0]},${Number(data[1]) + i}"]`
-    );
-    square.classList.remove("ship-four");
-  }
-};
-
+//   setTimeout(() => {
+//     placePoint.removeChild(ship);
+//   }, 100);
+// };
 const playerSquares = document.querySelectorAll("#player-board > .square");
+
 
 playerSquares.forEach((el) =>
   el.addEventListener("mouseover", () => highlight(el, 4))
